52f42a5d0a2284e512566a6abf59cb331b754d9d
Kilka <Keilija@mail.ru>
2022-02-02 20:10:33 +0400

    002

---
+++ 002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите имя для приветствия");
+string username = Console.ReadLine();
+Console.Write("Не то что для мення было бы важно твоё имя. Но  привет, ");
+Console.WriteLine(username);
