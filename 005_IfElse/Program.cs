cf507afd7ec282f96d61c1e6a26928ee57fbddd3
Kilka <Keilija@mail.ru>
2022-02-02 21:55:04 +0400

    005 лучшенное привествие

---
+++ 005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя для приветствия");
+string username = Console.ReadLine();
+if (username.ToLower() == "килька")
+{
+   Console.Write("Привет Килька, держи сердешко");
+}
+else
+{
+    Console.Write("Не то что для мення было бы важно твоё имя. Но  привет, ");
+    Console.WriteLine(username);
+}    
+    
+    
