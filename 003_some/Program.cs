2adccd46de61c314779d05f2354edc84823e9989
Kilka <Keilija@mail.ru>
2022-02-02 21:19:17 +0400

    002 добавили рандом

--- 003_some/Program.cs
+++ 003_some/Program.cs
@@ -1,3 +1,8 @@
-﻿int namderA =3;
-int namderB =2;
+﻿int namderA = new Random().Next(1,10);
+int namderB =new Random().Next(1,10);
+Console.Write("namderA =");
+Console.WriteLine(namderA);
+Console.Write("namderB =");
+Console.WriteLine(namderB);
+Console.Write("namderA + namderB =");
 Console.WriteLine(namderA + namderB);
