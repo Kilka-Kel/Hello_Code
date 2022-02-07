e441f2338ae76a9e9782053f5dbf344bc267f97a
Kilka <Keilija@mail.ru>
2022-02-07 22:00:34 +0400

    008 семинар 1. утройсто ввода данных- клавиатура

---
+++ 008 VvodChisla/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int namderA = 1;
+int namderB = 1;
+string s;
+Console.Write("Введите число А ");
+s=Console.ReadLine();
+namderA = Convert.ToInt32(s);
+Console.Write("Введите число B ");
+s=Console.ReadLine();
+namderB = Convert.ToInt32(s);
+Console.Write("A + B =");
+Console.WriteLine(namderA + namderB);
+
